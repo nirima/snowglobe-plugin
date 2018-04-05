77a0298eaac863245280777e8c1a78cef011e6b5
Nigel Magnay <nigel.magnay@gmail.com>
2018-04-05 13:12:09 +0100

    So that PRs being deleted can also delete the snowglobe, listen via an ItemListener.
    Signed-off-by: Nigel Magnay <nigel.magnay@gmail.com>

---
+++ src/main/java/com/nirima/snowglobe/jenkins/SnowGlobeItemListener.java
@@ -0,0 +1,29 @@
+package com.nirima.snowglobe.jenkins;
+
+import com.nirima.snowglobe.jenkins.actions.SnowGlobeAction;
+
+import org.jenkinsci.plugins.workflow.job.WorkflowJob;
+
+import hudson.Extension;
+import hudson.model.Actionable;
+import hudson.model.Failure;
+import hudson.model.Item;
+import hudson.model.ItemGroup;
+import hudson.model.Run;
+import hudson.model.listeners.ItemListener;
+
+@Extension
+public class SnowGlobeItemListener extends ItemListener {
+
+  @Override
+  public void onDeleted(Item item) {
+    super.onDeleted(item);
+
+    if( item instanceof Actionable) {
+      SnowGlobeAction action  = ((Actionable)item).getAction(SnowGlobeAction.class);
+      if( action != null ) {
+        action.onDelete();
+      }
+    }
+  }
+}
